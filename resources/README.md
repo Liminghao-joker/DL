71b3d8f06c03853118de0451aae0e94c4361fc9d
Li MH <2654330367@qq.com>
2025-09-14 11:43:02 +0800

    update resources
    添加入门深度学习过程中的线上资源

---
+++ resources/README.md
@@ -0,0 +1,27 @@
+收纳整理入门深度学习过程中的线上资料。  
+
+深度学习入门（鱼书）
+- **PDF**
+- **code**
+
+Pytorch入门（小土堆）
+[PyTorch深度学习快速入门](https://www.bilibili.com/video/BV1hE411t7RN/?spm_id_from=333.337.search-card.all.click&vd_source=47dbec3f3db6a86044a31f482a95d4f0)
+
+RethinkFun
+- [源代码](https://github.com/RethinkFun/DeepLearning)
+- [电子书](https://www.rethink.fun/)
+
+刘二大人
+- **slides**
+- [PyTorch深度学习实践](https://www.bilibili.com/video/BV1Y7411d7Ys/?spm_id_from=333.337.search-card.all.click&vd_source=47dbec3f3db6a86044a31f482a95d4f0)
+
+动手学深度学习(Pytorch)
+- [电子书](https://zh.d2l.ai/)
+- [视频](https://www.bilibili.com/video/BV1if4y147hS/?spm_id_from=333.337.search-card.all.click&vd_source=47dbec3f3db6a86044a31f482a95d4f0)
+
+李宏毅《深度学习》2021/2022
+- **PDF** LeeDL_Tutorial_v.1.2.4
+- [李宏毅2021/2022春机器学习课程](https://www.bilibili.com/video/BV1Wv411h7kN/?spm_id_from=333.337.search-card.all.click&vd_source=47dbec3f3db6a86044a31f482a95d4f0))
+
+3Blue1Brown
+[3Blue1Brown 科普视频](https://www.bilibili.com/video/BV1bx411M7Zx?spm_id_from=333.788.recommend_more_video.2&vd_source=47dbec3f3db6a86044a31f482a95d4f0)
---
+++ resources/深度学习入门（鱼书）/code/common/gradient.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+import numpy as np
+
+def _numerical_gradient_1d(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 还原值
+        
+    return grad
+
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
+        
+        return grad
+
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    # flags = ['multi_index']: 允许获取当前元素的多维索引
+    # op_flags = ['readwrite']: 允许修改当前元素的值
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 还原值
+        it.iternext()   
+        
+    return grad
